299cbe7360ec2cad9fb17d6c6d8a7cea7adee38d
Francisco Maseda Muiño <frmadem@gmail.com>
2022-02-07 13:38:54 +0000

    Fix problems on image

--- artifacts/docker/amqp_consumer_task/index.js
+++ artifacts/docker/amqp_consumer_task/index.js
@@ -1,5 +1,5 @@
 
-const amqp = require('amqplib')
+const amqp = require('amqplib/callback_api')
 
 const config = require("config")
 
@@ -7,22 +7,44 @@ const opt = {
   credentials: amqp.credentials.plain(config.get("amqp.user"), config.get("amqp.password")) 
 }
 
-amqp.connect(config.get("amqp.server"), opt, function(err, connection) {
-  if (err) {
-    throw err;
-  }
-  
-  connection.createChannel(function(error, channel) {
-  
-    if (error) {
-      throw error;
-    }
+function createConnection(){
+
+  return new Promise(function(ok, ko){
+
+    amqp.connect(config.get("amqp.server"), opt, function(error, connection){
+
+      if(error){
+
+        throw error
+      }
+
+      connection.createChannel(function(error1, channel){
+
+        if(error1){
+
+          throw error1
+        }
+
+        const queue = config.get("amqp.queue")
+
+        channel.assertQueue(queue, {
+
+
+        })
+
+        ok(channel)
+
+      })
+
+    })
+  })
+}
+
+createConnection()
+
+  .then((channel) => {
 
     const queue = config.get("amqp.queue")
-
-    channel.assertQueue(queue, {
-      durable: true
-    });
 
     channel.prefetch(1);
 
@@ -39,4 +61,41 @@ amqp.connect(config.get("amqp.server"), opt, function(err, connection) {
     });
   
   });
-});
+
+//amqp.connect(config.get("amqp.server"), opt, function(err, connection) {
+//
+//  if (err) {
+//    throw err;
+//  }
+//  
+//  connection.createChannel(function(error, channel) {
+//  
+//    if (error) {
+//      throw error;
+//    }
+//
+//    const queue = config.get("amqp.queue")
+//
+//    channel.assertQueue(queue, {
+//
+//      durable: true
+//
+//    });
+//
+//    channel.prefetch(1);
+//
+//    channel.consume(queue, function(msg) {
+//
+//      console.log("Recibido '%s'", msg.content.toString())
+//
+//      setTimeout(function() {
+//  
+//        channel.ack(msg);
+//  
+//      }, config.get("amqp.consume_rate"))
+//  
+//    });
+//  
+//  });
+//
+//});
